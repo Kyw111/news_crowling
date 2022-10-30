0e7d4679bee4733ea3e6fe8ad464c52d8fb867a3
KIM YOUNG WOO <partan5260@naver.com>
2022-10-31 00:44:32 +0900

    카카오 메시지 API 연동 - 인가 코드 활용 및 token값 받아와서 카톡 메시지 전송까지 ok

--- src/main/java/com/example/demo/Constant.java
+++ src/main/java/com/example/demo/Constant.java
@@ -3,15 +3,7 @@ package com.example.demo;
 public class Constant {
 
     public static final String AUTH_URL = "https://kauth.kakao.com/oauth/token";
-//    public static final String AUTH_URL =
-//        "https://kauth.kakao.com/oauth/authorize?client_id=9ba73a5d82c46310d488d084373c1311&response_type=code&redirect_uri=http://localhost:8080/issue&client_secret=dmuuGnvDRvGsOYG3mwvmb9zYjrSY9qwr";
 
 
-    public static final String KAKAO_REDIRECT_URL = "http://localhost:8080/issue";
-
-    public final static String KAKAO_API_KEY = "9ba73a5d82c46310d488d084373c1311";
-    public final static String CLIENT_SECRET_KEY = "dmuuGnvDRvGsOYG3mwvmb9zYjrSY9qwr";
-
-    public final static String TOKEN = "lWMtPUbz9kLJcc3ATSByoj2rZsIZ9rhJeHa6N0d76Bkr0BF4FSubmGNt3nC8V14nsox4MAo9c5sAAAGEJ3AFfg";
 
 }
